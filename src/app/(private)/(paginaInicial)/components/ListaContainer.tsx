d088b796dcdf0711c0cbbd259c1906ace14b37b3
Júlio Ricardo Milcharek Cavalheiro <juliomilcharek519@gmail.com>
2025-05-04 23:29:04 -0300

    pequena modificação

--- src/app/(private)/(paginaInicial)/components/ListaContainer.tsx
+++ src/app/(private)/(paginaInicial)/components/ListaContainer.tsx
@@ -9,7 +9,7 @@ export default function ListaContainer({titulo, subtitulo, className, children}:
         <div className={`flex flex-col gap-[20px] bg-branco-100 rounded-[20px] p-[20px] md:p-[30px] ${className}`}
              >
             <div>
-                <h1 className="text-titulo-card-2">{titulo}</h1>
+                <h1 className="text-titulo-card-2 md-">{titulo}</h1>
                 <h2 className="text-texto-lista text-cinza-300">{subtitulo}</h2>
             </div>
             <div>
