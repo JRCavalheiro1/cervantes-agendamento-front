d516a9004cf400dfd7805b9a9e3f3c44883da406
Júlio Ricardo Milcharek Cavalheiro <juliomilcharek519@gmail.com>
2025-05-14 17:06:51 -0300

    useServico

---
+++ src/features/servico/hooks/use-servico.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import { useState } from "react";
+import { ServicoType } from "@/features/servico/types/servico";
+
+export function useServico() {
+  const [servicos, setServicos] = useState<ServicoType[]>([]);
+
+  const adicionaServico = (novoServico: Omit<ServicoType, "id">) => {
+    const novoServicoComId = {
+      ...novoServico,
+      id: Math.random().toString(36).substring(2, 9),
+    };
+
+    setServicos([...servicos, novoServicoComId]);
+    return novoServicoComId;
+  };
+}
---
+++ src/features/servico/types/servico.ts
@@ -0,0 +1,8 @@
+export type ServicoType = {
+  id: string;
+  nome: string;
+  descricao: string;
+  duracao: number;
+  preco: number;
+  imagem: File;
+};
