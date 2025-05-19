b7e81949d8d808283d225cc4a2674a66725b1ae9
Júlio Ricardo Milcharek Cavalheiro <juliomilcharek519@gmail.com>
2025-05-19 08:37:27 -0300

    modificaçÃões nos componentes de lista

---
+++ src/features/profissional/listas/lista-profissionais-ui.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import ProfissionalItem from "@/features/profissional/items/profissional-item";
+import ListaContainer from "@/components/sections/listas/lista-container";
+import { useTranslations } from "next-intl";
+import { ModalProfissional } from "@/features/profissional/modais/modal-profissional";
+import { useModal } from "@/hooks/utils/use-modal";
+import { ProfissionalProps } from "@/data/profissionais";
+import { ListaVazia } from "@/components/sections/listas/lista-vazia";
+import Link from "next/link";
+
+interface ListaProfissionaisUiProps {
+  variant?: "home";
+  profissionais: ProfissionalProps[];
+}
+export default function ListaProfissionaisUi({
+  variant,
+  profissionais,
+}: ListaProfissionaisUiProps) {
+  const {
+    itemSelecionado: profissionalSelecionado,
+    modalAberto,
+    abreModal,
+    fechaModal,
+  } = useModal<ProfissionalProps>();
+
+  const translationListaProf = useTranslations("ProfessionalList");
+
+  return (
+    <ListaContainer
+      titulo={translationListaProf("title")}
+      subtitulo={translationListaProf("subtitle")}
+      className="xl:flex-1"
+    >
+      {profissionais.length > 0 ? (
+        profissionais.map((item) => {
+          return (
+            <li key={item.id}>
+              <ProfissionalItem
+                {...item}
+                onClick={() => abreModal(item)}
+                variant={variant}
+              />
+            </li>
+          );
+        })
+      ) : (
+        <ListaVazia
+          icone={true}
+          descricao={translationListaProf("emptyListDescription")}
+        >
+          <Link
+            className="text-azul-300 text-texto-lista md:text-texto-form"
+            href="/profissionais"
+          >
+            {translationListaProf("linkAddNow")}
+          </Link>
+        </ListaVazia>
+      )}
+      {modalAberto && profissionalSelecionado && (
+        <ModalProfissional
+          profissional={profissionalSelecionado}
+          onClose={fechaModal}
+          open={modalAberto}
+        />
+      )}
+    </ListaContainer>
+  );
+}
--- src/features/servico/listas/lista-servicos.tsx
+++ src/features/servico/listas/lista-servicos.tsx
@@ -1,50 +1,6 @@
-"use client";
-import ListaContainer from "@/components/sections/listas/lista-container";
-import { useTranslations } from "next-intl";
-import { ListaVazia } from "@/components/sections/listas/lista-vazia";
-import { Toolbox } from "@phosphor-icons/react/dist/ssr";
-import { servicos, ServicosProps } from "@/data/servicos";
-import { ServicoItem } from "@/features/servico/items/servico-item";
-import { useModal } from "@/hooks/utils/use-modal";
-import { ModalServico } from "@/features/servico/modais/modal-servico";
+import { servicos } from "@/data/servicos";
+import ListaServicosUi from "@/features/servico/listas/lista-servico-ui";
 
 export default function ListaServicos() {
-  const listaServicosTranslation = useTranslations("Services");
-  const {
-    itemSelecionado: servicoSelecionado,
-    modalAberto,
-    abreModal,
-    fechaModal,
-  } = useModal<ServicosProps>();
-
-  return (
-    <ListaContainer
-      titulo={listaServicosTranslation("title")}
-      subtitulo={listaServicosTranslation("subtitle")}
-    >
-      {servicos.length > 0 ? (
-        servicos.map((servico) => (
-          <li key={servico.id}>
-            <ServicoItem
-              nome={servico.nome}
-              imagem={servico.imagem}
-              onClick={() => abreModal(servico)}
-            />
-          </li>
-        ))
-      ) : (
-        <ListaVazia
-          icone={false}
-          descricao={listaServicosTranslation("emptyListDescription")}
-        />
-      )}
-      {modalAberto && servicoSelecionado && (
-        <ModalServico
-          servico={servicoSelecionado}
-          open={modalAberto}
-          onClose={fechaModal}
-        />
-      )}
-    </ListaContainer>
-  );
+  return <ListaServicosUi servicos={servicos} />;
 }
